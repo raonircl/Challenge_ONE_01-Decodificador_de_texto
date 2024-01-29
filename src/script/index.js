b49098d4c51f026bc87711e450d7f90e8c134f0c
raonircl <raoni.afi@gmail.com>
2024-01-28 21:29:53 -0300

    feat: função de descriptografar e footer

--- src/script/index.js
+++ src/script/index.js
@@ -3,14 +3,51 @@ const criptografarClick = () => {
     const exibirTexto = document.getElementById('exibirtexto');
     const rectangle = document.getElementById('rectangle-1');
     const click = document.getElementById('button1');
-
+    
     click.addEventListener('click', function () {
         const textoCriptografado = texto.value.toLowerCase();
+        console.log(textoCriptografado);
+        rectangle.classList.add('hidden');
+        exibirTexto.removeAttribute('hidden');
+        exibirTexto.textContent = criptografia(textoCriptografado);
+    });
+};
 
+const descriptografarClick = () => {
+    const texto = document.getElementById('textarea');
+    const exibirTexto = document.getElementById('exibirtexto');
+    const rectangle = document.getElementById('rectangle-1');
+    const click = document.getElementById('button2');
 
+    click.addEventListener('click', function () {
+        const textoDescriptografado = texto.value.toLowerCase();
 
         rectangle.classList.add('hidden');
         exibirTexto.removeAttribute('hidden');
-        exibirTexto.textContent = texto.value;
+        exibirTexto.textContent = descriptografia(textoDescriptografado);
     });
+};
+
+const criptografia = (texto) => {
+    const mapaDeCriptografia = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+
+    return texto.replace(/[eioua]/g, letra => mapaDeCriptografia[letra]);
+};
+
+const descriptografia = (texto) => {
+    const mapaDeCriptografia = {
+        'enter': 'e',
+        'imes': 'i',
+        'ai': 'a',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+
+    return texto.replace(/(enter|imes|ai|ober|ufat)/g, palavra => mapaDeCriptografia[palavra]);
 };
